c7399083d5cc197f913a6c3f9e5d54cdcd72eac8
Weston C. Beecroft <westoncb@gmail.com>
2018-06-03 17:57:22 -0700

    Initial test of tracing shapes with the 'laser'

--- laser.js
+++ laser.js
@@ -6,34 +6,17 @@ class Laser {
 	constructor() {
 		this.options = {
 						position: new THREE.Vector3(),
-						positionRandomness: .3,
 						velocity: new THREE.Vector3(),
-						velocityRandomness: .5,
 						color: 0xaa88ff,
-						colorRandomness: .2,
-						turbulence: .5,
-						lifetime: 2,
+						lifetime: 1,
 						size: 5,
-						sizeRandomness: 1
+						timeScale: 1,
 					};
 
-		this.spawnerOptions = {
-			spawnRate: 15000,
-			horizontalSpeed: 1.5,
-			verticalSpeed: 1.33,
-			timeScale: 1
-		};
-
-		this.datgui = new dat.GUI( { width: 351 } );
-		this.datgui.add( this.options, "velocityRandomness", 0, 3 );
-		this.datgui.add( this.options, "positionRandomness", 0, 3 );
+		this.datgui = new dat.GUI( { width: 350 } );
 		this.datgui.add( this.options, "size", 1, 20 );
-		this.datgui.add( this.options, "sizeRandomness", 0, 25 );
-		this.datgui.add( this.options, "colorRandomness", 0, 1 );
 		this.datgui.add( this.options, "lifetime", .1, 10 );
-		this.datgui.add( this.options, "turbulence", 0, 1 );
-		this.datgui.add( this.spawnerOptions, "spawnRate", 10, 30000 );
-		this.datgui.add( this.spawnerOptions, "timeScale", -1, 1 );
+		this.datgui.add( this.options, "timeScale", .01, 5 );
 
 		this.particleSystem = new ParticleSystem( {
 						maxParticles: 250000
@@ -43,22 +26,49 @@ class Laser {
 		this.obj3d.add( this.particleSystem );
 
 		this.tick = 0;
+		this.frame = 0;
+		this.offset = new THREE.Vector3();
+	}
+
+	move(destination) {
+		this.options.position.copy(destination);
+	}
+
+	trace(destination) {
+		const spawnDistance = 0.03;
+		const toDestVec = destination.clone().sub(this.options.position);
+		const distance = toDestVec.length();
+		const direction = toDestVec.normalize();
+
+		const spawns = distance / spawnDistance;
+
+		for (let i = 0; i < spawns; i++) {
+			this.options.position.addScaledVector(direction, spawnDistance);
+			this.particleSystem.spawnParticle( this.options );
+		}
+	}
+
+	execute() {
+		for (let i = -5; i < 5; i++) {
+			for (let j = -9; j < 9; j+=6) {
+				const k = 1;
+				this.move(new THREE.Vector3(0 + j, 0 - k, i).add(this.offset));
+				this.trace(new THREE.Vector3(5 + j, 5 - k, i).add(this.offset));
+				this.trace(new THREE.Vector3(5 + j, 0 - k, i).add(this.offset));
+				this.trace(new THREE.Vector3(0 + j, 0 - k, i).add(this.offset));
+			}
+		}
+
+		this.offset.x += 0.01;
 	}
 
 	update(deltaTime) {
-		const delta = deltaTime * this.spawnerOptions.timeScale;
+		const delta = deltaTime * this.options.timeScale;
 		this.tick += delta;
-		if ( this.tick < 0 ) this.tick = 0;
-		if ( delta > 0 ) {
-			this.options.position.x = Math.sin( this.tick * this.spawnerOptions.horizontalSpeed ) * 20;
-			this.options.position.y = Math.sin( this.tick * this.spawnerOptions.verticalSpeed ) * 10;
-			this.options.position.z = Math.sin( this.tick * this.spawnerOptions.horizontalSpeed + this.spawnerOptions.verticalSpeed ) * 5;
-			for ( let x = 0; x < this.spawnerOptions.spawnRate * delta; x++ ) {
-				// Yep, that's really it.	Spawning particles is super cheap, and once you spawn them, the rest of
-				// their lifecycle is handled entirely on the GPU, driven by a time uniform updated below
-				this.particleSystem.spawnParticle( this.options );
-			}
+		if (this.frame % 3 === 0) {
+			this.execute();
 		}
+		this.frame++;
 		this.particleSystem.update( this.tick );
 	}
 }
